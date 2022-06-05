fbbdf455740fd489bf5025913a897b055582f161
Jesse Buhagiar <jooster669@gmail.com>
2022-06-05 15:14:59 +1000

    Meta: Add basic CMake Project files

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+
+#include <cstdio>
+
+int main(void)
+{
+    printf("Hello, World!");
+    return 0;
+}
