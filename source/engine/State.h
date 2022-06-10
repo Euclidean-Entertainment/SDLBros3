7cfcb0c36db27842abac724572b573463dfebc6c
Jesse Buhagiar <jooster669@gmail.com>
2022-06-11 00:37:45 +1000

    Engine: Add base `State` class

---
+++ source/engine/State.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+
+#include <SDL2/SDL.h>
+#include "engine/Game.h"
+
+namespace Engine {
+
+class Game;
+class State
+{
+public:
+    State(Game& game) : m_game(game) {}
+    State(State const& rhs) = delete;
+    State(State&&) = delete;
+    virtual ~State() {}
+
+    virtual void handle_input(SDL_Event const& event) = 0;
+    virtual void update(float dt) = 0;
+    virtual void render() = 0;
+
+protected:
+    Game& m_game;
+};
+
+};
+
+using Engine::State;
