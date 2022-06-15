e2eb0a157af35af569a4ef84e6917e94d99db3d5
Jesse Buhagiar <jooster669@gmail.com>
2022-06-15 23:27:48 +1000

    Gfx: Add `FontRenderer` class

---
+++ source/gfx/FontRenderer.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include <gfx/FontRenderer.h>
+#include <system/Log.h>
+#include <SDL2/SDL.h>
+#include <algorithm>
+
+namespace GFX {
+
+// Font sprite sheet x-index Look Up Table
+static const int character_index_lut[59] =
+{
+    0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,        // 0x20-0x2F
+    2,3,4,5,6,7,8,9,10,11,                  // 0x30-0x39
+    0,0,0,0,0,0,0,                          // 0x3A-0x41
+    12,13,14,15,16,17,18,19,20,21,22,
+    23,24,25,26,27,28,29,30,31,32,33,
+    34,35,36
+};
+
+FontRenderer::FontRenderer(ResourceLoader& loader)
+{
+    (void)m_font_sheet.load(loader, "./data/sheets/font.png", "glyphs");
+}
+
+void FontRenderer::draw_string(SDL_Renderer* renderer, int x, int y, std::string const& message)
+{
+    if (m_font_sheet.texture().loaded() == false)
+    {
+        return;
+    }
+
+    auto total_width = static_cast<int>(message.length() * GLYPH_WIDTH);
+    SDL_Texture* string_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, total_width, GLYPH_HEIGHT);
+    SDL_Rect dest_rect = { 0, 0, GLYPH_WIDTH, GLYPH_HEIGHT };
+    auto sdl_rc = SDL_SetRenderTarget(renderer, string_texture);
+    if (sdl_rc != 0)
+    {
+        log(LogLevel::INFO, "Failed to set render target for message draw!");
+        return;
+    }
+
+    SDL_RenderClear(renderer);
+    for (auto const& character : message)
+    {
+        SDL_Rect clip_rect = { 0, 0, GLYPH_WIDTH, GLYPH_HEIGHT };
+        auto character_lut_index = character - ' ';
+
+        // Convert a lower case character to an upper case one
+        // This is marked `unlikely` because we control the text data, which
+        // will all be upper case, but you never know!
+        if (islower(character)) [[unlikely]]
+            character_lut_index = toupper(character);
+
+        auto const sheet_x_offset = character_index_lut[character_lut_index];
+        clip_rect.x = GLYPH_WIDTH * sheet_x_offset;
+        SDL_RenderCopy(renderer, m_font_sheet.texture().texture(), &clip_rect, &dest_rect);
+
+        // Increment to the next slot to draw to
+        dest_rect.x += GLYPH_WIDTH;
+    }
+
+    sdl_rc = SDL_SetRenderTarget(renderer, nullptr);
+    if (sdl_rc != 0)
+    {
+        log(LogLevel::INFO, "Failed to set render target for message draw!");
+        return;
+    }
+
+    SDL_Rect from_rect = { 0, 0, total_width, GLYPH_HEIGHT };
+    SDL_Rect to_rect = { x, y, total_width, GLYPH_HEIGHT };
+    SDL_RenderCopy(renderer, string_texture, &from_rect, &to_rect);
+}
+
+};
---
+++ source/gfx/FontRenderer.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+
+#include <gfx/SpriteSheet.h>
+#include <string>
+
+namespace GFX {
+
+class FontRenderer final
+{
+    static constexpr uint8_t GLYPH_WIDTH = 8u;
+    static constexpr uint8_t GLYPH_HEIGHT = GLYPH_WIDTH;
+
+public:
+    FontRenderer(ResourceLoader& loader);
+    ~FontRenderer() = default;
+
+    void draw_string(SDL_Renderer* renderer, int x, int y, std::string const& message);
+
+private:
+    SpriteSheet m_font_sheet { GLYPH_WIDTH, GLYPH_HEIGHT };
+};
+
+};
