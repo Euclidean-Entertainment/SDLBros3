d4bd273897ee958d95844aeaa5fd2e809d9e2010
Jesse Buhagiar <jooster669@gmail.com>
2022-06-17 17:46:20 +1000

    Engine: Add `Assertions` header file

---
+++ source/Assertions.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+
+#include <cassert>
+
+#define ASSERT(expr) (assert(expr))
+#define ASSERT_NOT_REACHED assert(false)
