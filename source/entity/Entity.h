a7a457845d20f1009e844e8e0467dea2259880ce
Jesse Buhagiar <jooster669@gmail.com>
2022-06-19 23:22:25 +1000

    Entity: Add basic entity framework class
    Something to hack away at...

---
+++ source/entity/Entity.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022, Jesse Buhagiar <jooster669@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#pragma once
+
+#include <gfx/SpriteSheet.h>
+#include <math/Vec2.h>
+
+namespace Entity
+{
+
+class Entity
+{
+public:
+    Entity() = default;
+    virtual ~Entity(){}
+
+    virtual void update() = 0;
+    virtual void render() = 0;
+    virtual void move() = 0;
+
+    FloatVector2 const& position() const { return m_position; }
+    void set_position(FloatVector2 const& position) { m_position = position; }
+
+protected:
+    GFX::SpriteSheet m_sheet;
+    FloatVector2 m_position;
+    FloatVector2 m_velocity;
+};
+
+};
+
+using Entity::Entity;
