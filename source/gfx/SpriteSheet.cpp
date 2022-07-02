ab6d05e2692c0a1112e194d3776e569e314653fe
Jesse Buhagiar <jooster669@gmail.com>
2022-07-03 00:08:17 +1000

    Gfx: Change function signature for `SpriteSheet::draw`
    We should use a signed type for this so we don't need to do any nasty
    static_cast'ing

--- source/gfx/SpriteSheet.cpp
+++ source/gfx/SpriteSheet.cpp
@@ -20,22 +20,22 @@ bool SpriteSheet::load(ResourceLoader& loader, std::string const& path, std::str
     return m_texture.loaded();
 }
 
-void SpriteSheet::draw(SDL_Renderer* renderer, unsigned x, unsigned y, unsigned sheet_grid_x, unsigned sheet_grid_y)
+void SpriteSheet::draw(SDL_Renderer* renderer, int x, int y, int sheet_x, int sheet_y)
 {
     // Set up the SDL_Rect to slice out the appropriate sprite from the sheet
     SDL_Rect sheet_rect;
     SDL_Rect dest_rect;
 
     // Disgusting casts as SDL uses signed types to represent x and y(?)
-    sheet_rect.x = static_cast<int>(sheet_grid_x * m_xsize);
-    sheet_rect.y = static_cast<int>(sheet_grid_y * m_ysize);
-    sheet_rect.w = static_cast<int>(m_xsize);
-    sheet_rect.h = static_cast<int>(m_ysize);
+    sheet_rect.x = sheet_x * m_xsize;
+    sheet_rect.y = sheet_y * m_ysize;
+    sheet_rect.w = m_xsize;
+    sheet_rect.h = m_ysize;
 
-    dest_rect.x = static_cast<int>(x);
-    dest_rect.y = static_cast<int>(y);
-    dest_rect.w = static_cast<int>(m_xsize);
-    dest_rect.h = static_cast<int>(m_ysize);
+    dest_rect.x = x;
+    dest_rect.y = y;
+    dest_rect.w = m_xsize;
+    dest_rect.h = m_ysize;
 
     (void)SDL_RenderCopy(renderer, m_texture.texture(), &sheet_rect, &dest_rect);
 }
--- source/gfx/SpriteSheet.h
+++ source/gfx/SpriteSheet.h
@@ -22,7 +22,7 @@ public:
     bool load(ResourceLoader& loader, std::string const& path, std::string const& name);
     void set_xsize(uint8_t xsize) { m_xsize = xsize; }
     void set_ysize(uint8_t ysize) { m_ysize = ysize; }
-    void draw(SDL_Renderer* renderer, unsigned x, unsigned y, unsigned sheet_grid_x, unsigned sheet_grid_y);
+    void draw(SDL_Renderer* renderer, int x, int y, int sheet_x, int sheet_y);
 
     Texture const& texture() const { return m_texture; }
     uint8_t xsize() const { return m_xsize; }
