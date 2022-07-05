ee2438d83acfef93888203a2d9f0b38f8cca3367
Jesse Buhagiar <jooster669@gmail.com>
2022-07-05 21:08:36 +1000

    Tests: Get code coverage of actscript test to 100%

--- tests/engine/actscript_test.cpp
+++ tests/engine/actscript_test.cpp
@@ -34,6 +34,24 @@ TEST_CASE("Test ASF queue load and pump")
     CHECK(test_queue.count() == 0u);
 }
 
+TEST_CASE("Test ActionScript Queue enque events")
+{
+    Engine::ActScript::Queue test_queue;
+
+    test_queue.enqueue_event(Engine::ActScript::Queue::Event(0x1234u, 0x5678u));
+    test_queue.enqueue_event(0xabcdu, 0x12abu);
+
+    auto event1 = test_queue.dequeue();
+    CHECK(event1.command == 0x1234u);
+    CHECK(event1.delay == 0x5678u);
+
+    auto event2 = test_queue.dequeue();
+    CHECK(event2.command == 0xabcdu);
+    CHECK(event2.delay == 0x12abu);
+
+    CHECK(test_queue.count() == 0u);
+}
+
 TEST_CASE("Test ASF bad magic")
 {
     Engine::ActScript::Queue test_queue;
@@ -56,4 +74,20 @@ TEST_CASE("Test ASF corrupt size")
 
     CHECK(test_queue.load_from_file("./test_data/asf/corrupt_size.asf") == false);
     CHECK(test_queue.count() == 0u);
+}
+
+TEST_CASE("Test ASF bad event count")
+{
+    Engine::ActScript::Queue test_queue;
+
+    CHECK(test_queue.load_from_file("./test_data/asf/bad_event_count.asf") == false);
+    CHECK(test_queue.count() == 0u);
+}
+
+
+TEST_CASE("Test ASF non-existent file")
+{
+    Engine::ActScript::Queue test_queue;
+
+    CHECK(test_queue.load_from_file("i_dont_exist.asf") == false);
 }
