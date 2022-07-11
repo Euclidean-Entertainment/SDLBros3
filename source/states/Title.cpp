69cf305bff8a6e8c15192cf6ee2b771d1b2e1b94
Jesse Buhagiar <jooster669@gmail.com>
2022-07-12 00:07:31 +1000

    States: Add full title proof of concept
    The code for this is incredibly messy. There needs to be some kind of
    way to automate this.

--- source/states/Title.cpp
+++ source/states/Title.cpp
@@ -23,6 +23,23 @@ void TitleState::init_resources()
     m_floor.set_y(m_game.window_height() - m_floor.height());
 
     (void)m_curtain.load(m_game.resource_loader(), "./data/backgrounds/title/curtain.png", "curtain");
+
+    // Load the title
+    (void)m_title.load(m_game.resource_loader(), "./data/backgrounds/title/title.png", "smb3_title");
+    (void)m_three_sheet.load(m_game.resource_loader(), "./data/sheets/title/threes.png", "three");
+    m_three_animation.load("./data/agf/title/three_glow.agf");
+    m_three_animation.set_current_animation_index(0u);
+    m_three_sheet.set_xsize(42);
+    m_three_sheet.set_ysize(41);
+
+    // FIXME: Do something better than this fucking trash
+    m_title.set_y(28 - 0xD0);
+    m_three_x_position = (m_game.window_width() / 2) - (m_three_sheet.xsize() / 2);
+    m_three_y_position = ((28 + m_title.height()) - 0xD0);
+
+    // Load the background
+    (void)m_bg.load(m_game.resource_loader(), "./data/backgrounds/title/bg.png", "background");
+    m_bg.set_alpha(0);
 }
 
 void TitleState::init_animations()
@@ -53,13 +70,76 @@ void TitleState::update()
         else
         {
             m_should_raise_curtain = false;
+            m_should_drop_title = true;
         }
+    }
+
+    if (m_should_drop_title)
+    {
+        m_title_drop_vertical_count -= 5;
+        m_title.set_y(m_title.y() + 5);
+        m_three_y_position += 5;
+
+        if (m_title_drop_vertical_count <= 0)
+        {
+            m_should_shake = true;
+            m_should_drop_title = false;
+        }
+    }
+
+    if (m_should_shake)
+    {
+        if ((m_shake_counter % 2u) == 0u)
+        {
+            // Shake up
+            m_title.set_y(m_title.y() + 2);
+            m_three_y_position += 2;
+        }
+        else
+        {
+            // Shake down
+            m_title.set_y(m_title.y() - 2);
+            m_three_y_position -= 2;
+        }
+
+        m_shake_counter--;
+        if (m_shake_counter == 0u)
+        {
+            m_should_shake = false;
+            m_should_fade_in = true;
+        }
+    }
+
+    if (m_should_fade_in)
+    {
+        if (static_cast<uint16_t>(m_bg_alpha + 16u) >= 256u)
+        {
+            m_bg_alpha = 255u;
+            m_should_fade_in = false;
+            m_should_glow = true;
+        }
+        else
+        {
+            m_bg_alpha += 16u;
+        }
+
+        m_bg.set_alpha(m_bg_alpha);
+    }
+
+    if (m_should_glow)
+    {
+        m_three_animation.current_animation().tick();
     }
 }
 
 void TitleState::render()
 {
     m_floor.draw(m_renderer);
+    m_bg.draw(m_renderer);
+    m_title.draw(m_renderer);
+    m_three_sheet.draw(m_renderer, m_three_x_position, m_three_y_position,
+                       m_three_animation.current_animation().current_frame().sheet_rect.x,
+                       m_three_animation.current_animation().current_frame().sheet_rect.y);
     m_curtain.draw(m_renderer);
 }
 
--- source/states/Title.h
+++ source/states/Title.h
@@ -9,7 +9,9 @@
 #include <engine/Game.h>
 #include <engine/State.h>
 #include <engine/Timer.h>
+#include <gfx/AnimationGroup.h>
 #include <gfx/BackgroundImage.h>
+#include <gfx/SpriteSheet.h>
 #include <memory>
 
 namespace States {
@@ -36,11 +38,26 @@ private:
 private:
     BackgroundImage m_floor;
     BackgroundImage m_curtain;
+    BackgroundImage m_title;
+    BackgroundImage m_bg;
+    GFX::SpriteSheet m_three_sheet;
+    GFX::AnimationGroup m_three_animation;
 
     std::shared_ptr<Timer> m_curtain_timer { nullptr };
+    int16_t m_title_drop_vertical_count { 0xD0 };
+    uint16_t m_shake_counter { 32u };
+
+    int m_three_x_position { 0 };
+    int m_three_y_position { 0 };
+
+    uint8_t m_bg_alpha { 0u };
 
     // State flags
     bool m_should_raise_curtain { false };
+    bool m_should_drop_title { false };
+    bool m_should_shake { false };
+    bool m_should_fade_in { false };
+    bool m_should_glow { false };
 };
 
 } // namespace States
